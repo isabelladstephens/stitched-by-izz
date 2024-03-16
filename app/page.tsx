b41ee07aa656a3198334f096fa508b818a252804
Isabella Stephens <isabelladstephens@gmail.com>
2024-03-16 12:08:21 -0600

    More responsive for desktop screens

--- app/page.tsx
+++ app/page.tsx
@@ -6,23 +6,25 @@ import Portfolio from "./portfolio/page";
 
 export default function Home() {
   return (
-    <div className={styles.home_container}>
-      <main className={styles.home_main}>
-        <h1 className={styles.title}>stitched by izz</h1>
-        <p className={styles.subtitle}>sewing + fashion design</p>
-        <button className={styles.check_it_out}>
-          <a href="#about">
-            check it out
-          </a>
-        </button>
-      </main>
+    <div>
+      <div className={styles.home_container}>
+        <main className={styles.home_main}>
+          <h1 className={styles.title}>stitched by izz</h1>
+          <p className={styles.subtitle}>sewing + fashion design</p>
+          <button className={styles.check_it_out}>
+            <a href="#about">
+              check it out
+            </a>
+          </button>
+        </main>
+      </div>
       <div id="about">
         <About/>
-        <Portfolio/>
-        <Contact/>
-        <img src="/images/quote_image.png" className={styles.quote}/>
-        <Footer/>
       </div>
+      <Portfolio/>
+      <Contact/>
+      <img src="/images/quote_image.png" className={styles.quote}/>
+      <Footer/>
     </div>
   );
 }
